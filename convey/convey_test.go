8383627ef49a615ed5cb8d6d44c834fe8749583f
Andy McCall <andymc903@gmail.com>
2017-04-05 20:49:39 -0700

    convey test #1

--- convey/convey_test.go
+++ convey/convey_test.go
@@ -1,11 +1,33 @@
 package convey
 
 import (
-	"github.com/vektra/mockery/mocks"
 	"testing"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/vektra/mockery/mocks"
+	m "github.com/stretchr/testify/mock"
 )
 
 func TestNewFunc(t *testing.T)	{
-	
+	Convey( "given the blank mock" , t, func() {
+		mock := new(mocks.Blank)
+		mock.On("Create", m.Anything).Return(nil)
+		Convey("when create is clalled", func() {
+			err := mock.Create("test")
+			Convey("the mock doesn't return an error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("the mock can be asserted with https://github.com/smartystreets/goconvey", func() {
+				var output string
+				Convey("That func shouldn't panic", func() {
+					So(func() {
+						output = m.NumberOfCalls(mock, "Create", 1)
+					}, ShouldNotPanic)
+				})
+				Convey("That the test passed", func() {
+					So(output, ShouldBeBlank)
+				})
+			})
+		})
+	})
+
 }
